f2b5197dd8e80d32e8b5460489256b5fbb4f7f9e
gentleduo <gentleduo@163.com>
2022-02-16 22:06:31 +0800

    发射

--- src/main/java/org/duo/herostory/GameMsgRecognizer.java
+++ src/main/java/org/duo/herostory/GameMsgRecognizer.java
@@ -3,11 +3,17 @@ package org.duo.herostory;
 import com.google.protobuf.GeneratedMessageV3;
 import com.google.protobuf.Message;
 import org.duo.herostory.msg.GameMsgProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public final class GameMsgRecognizer {
+
+    static private final Logger LOGGER = LoggerFactory.getLogger(GameMsgRecognizer.class);
 
     static private final Map<Integer, GeneratedMessageV3> _msgCodeAndMsgObjMap = new HashMap<>();
 
@@ -21,6 +27,39 @@ public final class GameMsgRecognizer {
         // 获取GameMsgProtocol的内部类
         Class<?>[] declaredClasses = GameMsgProtocol.class.getDeclaredClasses();
 
+        for (Class<?> clazz : declaredClasses) {
+            if (clazz == null || !GeneratedMessageV3.class.isAssignableFrom(clazz)) {
+                continue;
+            }
+            String clazzName = clazz.getSimpleName();
+            clazzName = clazzName.toLowerCase();
+            for (GameMsgProtocol.MsgCode msgCode : GameMsgProtocol.MsgCode.values()) {
+                if (null == msgCode) {
+                    continue;
+                }
+                // 消息编码
+                String strMsgCode = msgCode.name();
+                strMsgCode = strMsgCode.replaceAll("_", "");
+                strMsgCode = strMsgCode.toLowerCase();
+
+                if (!strMsgCode.startsWith(clazzName)) {
+                    continue;
+                }
+
+                Object getDefaultInstance = null;
+                try {
+                    getDefaultInstance = clazz.getDeclaredMethod("getDefaultInstance").invoke(clazz);
+                } catch (IllegalAccessException e) {
+                    LOGGER.error(e.getMessage(), e);
+                } catch (InvocationTargetException e) {
+                    LOGGER.error(e.getMessage(), e);
+                } catch (NoSuchMethodException e) {
+                    LOGGER.error(e.getMessage(), e);
+                }
+                _msgCodeAndMsgObjMap.put(msgCode.getNumber(), (GeneratedMessageV3) getDefaultInstance);
+                _clazzAndMsgCodeMap.put(clazz, msgCode.getNumber());
+            }
+        }
 //        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.USER_ENTRY_CMD_VALUE, GameMsgProtocol.UserEntryCmd.getDefaultInstance());
 //        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.WHO_ELSE_IS_HERE_CMD_VALUE, GameMsgProtocol.WhoElseIsHereCmd.getDefaultInstance());
 //        _msgCodeAndMsgObjMap.put(GameMsgProtocol.MsgCode.USER_MOVE_TO_CMD_VALUE, GameMsgProtocol.UserMoveToCmd.getDefaultInstance());
--- src/main/java/org/duo/herostory/cmdhandler/CmdHandlerFactory.java
+++ src/main/java/org/duo/herostory/cmdhandler/CmdHandlerFactory.java
@@ -1,12 +1,17 @@
 package org.duo.herostory.cmdhandler;
 
 import com.google.protobuf.GeneratedMessageV3;
+import org.duo.herostory.GameMsgRecognizer;
 import org.duo.herostory.msg.GameMsgProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public final class CmdHandlerFactory {
+
+    static private final Logger LOGGER = LoggerFactory.getLogger(CmdHandlerFactory.class);
 
     static private Map<Class<?>, ICmdHandler<? extends GeneratedMessageV3>> _handlerMap = new HashMap<>();
 
